85b731a5b01ac8ec67cc61673dcc9ef05dadc654
nikolamat99 <121299803+nikolamat99@users.noreply.github.com>
2022-12-27 12:40:53 +0100

    Add files via upload

---
+++ js/slicksettings.js
@@ -0,0 +1,19 @@
+$('.team-carousel').slick({
+	arrows: false,
+    autoplaySpeed: 300,
+	infinite: true,
+	autoplay: true,
+	mobileFirst: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	responsive: [
+		{
+			breakpoint: 768,
+			settings: { slidesToShow: 2 },
+		},
+        {
+			breakpoint: 992,
+			settings: { slidesToShow: 3 },
+		}
+	],
+});
